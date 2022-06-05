8f8ec401bb16bbebf20863d0c1c1bfe650855c27
emma42069 <96257725+emma42069@users.noreply.github.com>
2022-06-06 00:13:07 +0530

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,12 +1,11 @@
-wget https://raw.githubusercontent.com/emma42069/mc/files.zip
+wget https://raw.githubusercontent.com/emma42069/mc/master/files.zip
 unzip files.zip -d files
 
-DIR = "$HOME/Desktop"
+DIR="$HOME/Desktop"
 
-if [ -d DIR ]; then
-	mv files "$DIR"
+if [ -d "$DIR" ]; then
+        mv files "$DIR"
 else
-	mkdir test
-	mv files test
+        mkdir test
+        mv files test
 fi
-
