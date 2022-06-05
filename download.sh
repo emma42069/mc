2e539d704e000961bd925e4720f8c0a8e1a7a05f
emma42069 <96257725+emma42069@users.noreply.github.com>
2022-06-06 00:07:53 +0530

    Add files via upload

---
+++ download.sh
@@ -0,0 +1,12 @@
+wget https://raw.githubusercontent.com/emma42069/mc/files.zip
+unzip files.zip -d files
+
+DIR = "$HOME/Desktop"
+
+if [ -d DIR ]; then
+	mv files "$DIR"
+else
+	mkdir test
+	mv files test
+fi
+
